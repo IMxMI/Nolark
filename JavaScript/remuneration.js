46b9ea6ed7e20cb3ed0107cf69e30ee291b4363f
IIMxMII <mxm.vernoux@gmail.com>
2023-01-18 19:54:40 +0100

    Réparation carte

--- JavaScript/remuneration.js
+++ JavaScript/remuneration.js
@@ -2,5 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
-
+const fix = 1100;
+let nbAncien = parseInt(window.document.querySelector("#num_ancien").value); // Int 
 
