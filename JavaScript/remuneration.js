40242b93baa1a83f8118a00e733ee50ee5994acb
IIMxMII <mxm.vernoux@gmail.com>
2023-01-26 23:34:17 +0100

    Simulateur prime livreur

--- JavaScript/remuneration.js
+++ JavaScript/remuneration.js
@@ -2,6 +2,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
-const fix = 1100;
-let nbAncien = parseInt(window.document.querySelector("#num_ancien").value); // Int 
 
+const distance = 0.01, distance_max = 900, ancien_fixe = 300, ancien_up = 30;
+let primeA = 0, primeD = 0;
+
+function prime_ancien(ancien){
+    if (ancien < 4){
+        return(primeA = 0);
+    }
+    else{
+        primeA = ancien_fixe ;
+
+        if (ancien > 4){
+            primeA = ancien_fixe + ((ancien-4)*ancien_up);
+            }
+        }
+    return primeA
+}
+
+function prime_distance(km){
+    primeD = distance*km;
+    if (primeD > 900){
+        primeD = 900;
+    }
+    return primeD
+}
+
+let prime = primeA + primeD;
