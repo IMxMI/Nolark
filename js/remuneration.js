bbb39be51c1940703304c66588c93eeec30aebe3
Maxime VERNOUX <mxm.vernoux@gmail.com>
2023-03-12 19:45:27 +0100

    ancienneté

---
+++ js/remuneration.js
@@ -0,0 +1,14 @@
+function ancienneté (time){
+    const fixe = 1100;
+    if (time > 5){
+        if (time > 10){
+            return fixe * 1.06;
+        }
+        else {
+            return fixe * 1.03;
+        }
+    }
+    else {
+        return fixe
+    }
+}
