bd9cfd435eb2aa14a4c2a6a672614627cbe1f78c
Maxime VERNOUX <mxm.vernoux@gmail.com>
2023-03-16 22:53:46 +0100

    listernet

---
+++ js/poste.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function () {
+    // Déclaration de l'index de parcours
+    let i;
+    // tabInputs est une collection de <input>
+    let tabInputs = window.document.querySelectorAll("input");
+    // Parcours de tabInputs en s'appuyant sur le nombre de <input>
+    for (i = 0; i < tabInputs.length; i++) {
+        // Ajout d'un Listener sur tous les <input> sur l'évènement onKeyUp
+        tabInputs[i].addEventListener("click", calcRemu);
+    }
+});
