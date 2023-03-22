d9b9af50c887f5f34b38a33e850e737471894570
IIMxMII <mxm.vernoux@gmail.com>
2023-03-22 14:11:00 +0100

    Correction TP9

--- JavaScript/remuneration.js
+++
@@ -1,7 +0,0 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
-const fix = 1100;
-let nbAncien = parseInt(window.document.querySelector("#num_ancien").value); // Int 
-
