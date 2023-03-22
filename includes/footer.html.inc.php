749f2e764c2a70dea16662bdcb2b320f94cc534d
IIMxMII <mxm.vernoux@gmail.com>
2023-03-22 15:54:23 +0100

    correction

--- includes/footer.html.inc.php
+++ includes/footer.html.inc.php
@@ -4,8 +4,8 @@
         echo date("Y"); ?> Nolark - Créé par <a
             href="mailto:mxm.vernoux@gmail.com">Maxime Vernoux</a>
     </p>
-    <p> Nous somme le :
+    <p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspNous somme le :
     <?php date_default_timezone_set('Europe/Paris');
-        echo date('d-m-Y h:i:s'); ?>
+        echo date('d-m-Y  à  H:i'); ?>
     </p>
 </footer>
