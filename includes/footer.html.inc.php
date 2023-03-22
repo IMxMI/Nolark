9a3064f47b06c5d3cee7e6b2a5c3b9cbaa211891
IIMxMII <mxm.vernoux@gmail.com>
2023-03-22 15:39:34 +0100

    date

--- includes/footer.html.inc.php
+++ includes/footer.html.inc.php
@@ -1,4 +1,4 @@
 <footer>
-        <p>&copy;2018 Nolark - Créé par <a href="mailto:mxm.vernoux@gmail.com">Maxime Vernoux</a>
+        <p>&copy;<?php echo date("Y"); ?> Nolark - Créé par <a href="mailto:mxm.vernoux@gmail.com">Maxime Vernoux</a>
         <p>
     </footer>
