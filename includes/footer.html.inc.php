c3e5e32ce883f1cbaf8bc4890bda417a82407227
IIMxMII <mxm.vernoux@gmail.com>
2023-03-22 15:44:37 +0100

    date & heure

--- includes/footer.html.inc.php
+++ includes/footer.html.inc.php
@@ -1,4 +1,11 @@
 <footer>
-        <p>&copy;<?php echo date("Y"); ?> Nolark - Créé par <a href="mailto:mxm.vernoux@gmail.com">Maxime Vernoux</a>
-        <p>
-    </footer>
+    <p>&copy;
+        <?php date_default_timezone_set('Europe/Paris');
+        echo date("Y"); ?> Nolark - Créé par <a
+            href="mailto:mxm.vernoux@gmail.com">Maxime Vernoux</a>
+    </p>
+    <p> Nous somme le :
+    <?php date_default_timezone_set('Europe/Paris');
+        echo date('d-m-Y h:i:s'); ?>
+    </p>
+</footer>
