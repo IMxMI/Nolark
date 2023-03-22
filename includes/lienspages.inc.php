8b5cd3ebed598fea553e84e48062cb3148bbad9e
IIMxMII <mxm.vernoux@gmail.com>
2023-03-22 16:43:08 +0100

    WIP

--- includes/lienspages.inc.php
+++ includes/lienspages.inc.php
@@ -1,11 +1,21 @@
 <?php
+$scriptName = filter_input(INPUT_SERVER, 'SCRIPT_FILENAME');
+$name = substr($scriptName, strripos($scriptName, '/') + 1);
+if ($name === 'index.php') {
+    $dirIndex = './';
+    $dirPages = './pages/';
+} else {
+    $dirIndex = '../';
+    $dirPages = './';
+}
 $navpages = array(
-    'route.html' => 'Route',
-    'cross.html' => 'Cross',
-    'piste.html' => 'Piste',
-    'enfant.html' => 'Enfants',
-    'nous-contacter.html' => 'Nous contacter'
+    'Route' => 'route.php',
+    'Cross' => 'cross.php',
+    'Piste' => 'piste.php',
+    'Enfant' => 'enfant.php',
+    'Nous contacter' => 'nous-contacter.php'
 );
 foreach ($navpages as $cle => $val) {
     echo '<li><a href="pages/', $cle, '">', $val, '</a></li>';
 }
+//substr strrpos
