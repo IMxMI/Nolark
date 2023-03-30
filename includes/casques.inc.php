6420f34f04bb8e3258b8f05b974c943457cbdb8f
Maxime Vernoux <mxm.vernoux@gmail.com>
2023-03-30 22:43:46 +0200

    Correction

--- includes/casques.inc.php
+++ includes/casques.inc.php
@@ -1,6 +1,6 @@
 <?php
 //instancier l'objet cnx avec la source de donnée, l'identifiant et le mdp d'accés à la bd.
-$cnx = new PDO('mysql:host=127.0.0.1;dbname=nolark', 'nolarkuser', 'nolarkpwd'); // ERREUR
+$cnx = new PDO('mysql:host=127.0.0.1;dbname=nolark', 'nolarkuser', 'nolarkpwd'); // Corriger
 
 //contenue de la requête.
 $req = 'SELECT casque.id, nom, modele, libelle, prix, classement, image, stock';
