486d0f7beae51bf1ddd0d2a8b2399a3c514993e9
Maxime Vernoux <mxm.vernoux@gmail.com>
2023-03-30 21:38:04 +0200

    requête

--- includes/casques.inc.php
+++ includes/casques.inc.php
@@ -1,3 +1,8 @@
 <?php
 //instancier l'objet cnx avec la source de donnée, l'identifiant et le mdp d'accés à la bd.
 $cnx = new PDO('mysql:host=127.0.0.1;dbname=nolark', 'nolarkuser', 'nolarkpwd');
+
+//contenue de la requête.
+$req = 'SELECT casque.id, nom, modele, libelle, prix, classement, image, stock';
+$req .= ' FROM casque INNER JOIN type ON casque.type=type.id';
+$req .= ' INNER JOIN marque ON casque.marque=marque.id';
