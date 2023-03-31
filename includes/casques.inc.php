1e067f261a8e578d565274be5c746eb9ef63c209
Maxime Vernoux <mxm.vernoux@gmail.com>
2023-03-31 10:49:30 +0200

    correction TP11

--- includes/casques.inc.php
+++ includes/casques.inc.php
@@ -1,68 +1,37 @@
 <?php
-//instancier l'objet cnx avec la source de donnée, l'identifiant et le mdp d'accés à la bd.
-$cnx = new PDO('mysql:host=127.0.0.1;dbname=nolark', 'nolarkuser', 'nolarkpwd'); // Corriger
 
-//contenue de la requête.
-$req = 'SELECT casque.id, nom, modele, libelle, prix, classement, image, stock';
-$req .= ' FROM casque INNER JOIN type ON casque.type=type.id';
-$req .= ' INNER JOIN marque ON casque.marque=marque.id';
+$cnx = new PDO('mysql:host=127.0.0.1;dbname=nolark', 'nolarkuser', 'nolarkpwd');
+$req = 'SELECT casque.id, nom, modele, libelle, prix, classement, image, stock ';
+$req .= 'FROM casque INNER JOIN type ON casque.type=type.id ';
+$req .= 'INNER JOIN marque ON casque.marque=marque.id ';
+$req .= 'WHERE libelle = "' . substr($pageActuelle, 0, -4) . '"';
 
-//envoi de la requette au serveur.
 $res = $cnx->query($req);
 
-$scriptName = filter_input(INPUT_SERVER, 'SCRIPT_NAME');
-$pageActuelle = substr($scriptName, strrpos($scriptName, '/') + 1);
-
-//affiche les images apres avoir parcourue les résultats.
 echo '<section id="casques">';
 while ($ligne = $res->fetch(PDO::FETCH_OBJ)) {
-    if ($pageActuelle === 'route.php' and $ligne->libelle === 'route') {
-        echo '<article>';
-        echo '<img src="../images/casques/', $ligne->libelle, '/', $ligne->image,
-        '" alt="', $ligne->modele, '">';
-        echo '<p class="stockko"><abbr data-tip="',$ligne->stock,'">stock</abbr></p>';
-        echo '<p class="prix">',$ligne->prix,'</p>';
-        echo '<p class="marque">',$ligne->nom,'</p>';
-        echo '<p class="modele">',$ligne->modele,'</p>';
-        echo '</article>';
+    echo "\t", '<article>', "\n";
+    echo "\t\t", '<img src="../images/casques/', $ligne->libelle, '/', $ligne->image, '" alt="', $ligne->modele, '" >', "\n";
+
+    if ((int) $ligne->stock === -1) {
+        echo '<p class="stockko"><abbr data-tip="Sur commande uniquement">stock</abbr></p>';
+    } else if ((int) $ligne->stock === 0) {
+        echo '<p class="stockko"><abbr data-tip="Plus de casques en stock">stock</abbr></p>';
+    } else if ((int) $ligne->stock < 10) {
+        echo '<p class="stockok"><abbr data-tip="Plus que ', $ligne->stock, ' casques en stock...">stock</abbr></p>';
+    } else {
+        echo '<p class="stockok"><abbr data-tip="', $ligne->stock, ' casques en stock">stock</abbr></p>';
     }
-    elseif($pageActuelle === 'cross.php' and $ligne->libelle === 'cross') {
-        echo '<article>';
-        echo '<img src="../images/casques/', $ligne->libelle, '/', $ligne->image,
-        '" alt="', $ligne->modele, '">';
-        echo '<p class="stockko"><abbr data-tip="',$ligne->stock,'">stock</abbr></p>';
-        echo '<p class="prix">',$ligne->prix,'</p>';
-        echo '<p class="marque">',$ligne->nom,'</p>';
-        echo '<p class="modele">',$ligne->modele,'</p>';
-        echo '</article>';
+    echo "\t\t", '<p class="prix">', $ligne->prix, '€</p>', "\n";
+    echo "\t\t", '<p class="marque">', $ligne->nom, '</p>', "\n";
+    echo "\t\t", '<p class="modele">', $ligne->modele, '</p>', "\n";
+
+    // Si la note est de 5, il faut ajouter un 0 pour correspondre au selecteur CSS
+    if ($ligne->classement === 5) {
+        echo "\t\t", '<img class="classement classement0', $ligne->classement, '" src="../images/casques/etoiles.gif" alt="Classement ', str_replace('.', ',', $ligne->classement / 10), ' sur 5">', "\n";
+    } else {
+        echo "\t\t", '<img class="classement classement', $ligne->classement, '" src="../images/casques/etoiles.gif" alt="Classement ', str_replace('.', ',', $ligne->classement / 10), ' sur 5">', "\n";
     }
-    elseif($pageActuelle === 'enfants.php' and $ligne->libelle === 'enfants') {
-        echo '<article>';
-        echo '<img src="../images/casques/', $ligne->libelle, '/', $ligne->image,
-        '" alt="', $ligne->modele, '">';
-        echo '<p class="stockko"><abbr data-tip="',$ligne->stock,'">stock</abbr></p>';
-        echo '<p class="prix">',$ligne->prix,'</p>';
-        echo '<p class="marque">',$ligne->nom,'</p>';
-        echo '<p class="modele">',$ligne->modele,'</p>';
-        echo '</article>';
-    }
-    elseif($pageActuelle === 'piste.php' and $ligne->libelle === 'piste') {
-        echo '<article>';
-        echo '<img src="../images/casques/', $ligne->libelle, '/', $ligne->image,
-        '" alt="', $ligne->modele, '">';
-        echo '<p class="stockko"><abbr data-tip="',$ligne->stock,'">stock</abbr></p>';
-        echo '<p class="prix">',$ligne->prix,'</p>';
-        echo '<p class="marque">',$ligne->nom,'</p>';
-        echo '<p class="modele">',$ligne->modele,'</p>';
-        echo '</article>';
-    }
-    //<article>
-      //      <img src="../images/casques/cross/arai-vx3-frost-silver.jpg" alt="arai vx3 frost silver">
-        //    <p class="stockko"><abbr data-tip="Sur commande uniquement">stock</abbr></p>
-          //          echo '<p class="prix">',$ligne->prix,'</p>';
-            //<p class="marque">arai</p>
-        //    <p class="modele">vx3 frost silver</p>
-          //  <img class="classement classement20" src="../images/casques/etoiles.gif" alt="Classement 2 sur 5">
-        //</article>
+    echo "\t", '</article>', "\n";
 }
 echo '</section>';
